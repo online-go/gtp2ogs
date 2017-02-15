f4e672fd5371d30783493ca62fe60770b1581a84
Jonathan Roy <jonroy7@gmail.com>
2017-02-14 19:08:50 -0500

    Add a buffer for bot startup delay, if bot starts slow we could time out on first move

--- gtp2ogs.js
+++ gtp2ogs.js
@@ -54,6 +54,8 @@ let optimist = require("optimist")
     .describe('json', 'Send and receive GTP commands in a JSON encoded format')
     .describe('kgstime', 'Send time data to bot using kgs-time_settings command')
     .describe('noclock', 'Do not send any clock/time data to the bot')
+    .describe('startupbuffer', 'Subtract this many seconds from time available on first move')
+    .default('startupbuffer', 5)
 ;
 let argv = optimist.argv;
 
@@ -66,6 +68,10 @@ if (!argv._ || argv._.length == 0) {
 //
 if (argv.timeout) {
     argv.timeout = argv.timeout * 1000;
+}
+
+if (argv.startupbuffer) {
+    argv.startupbuffer = argv.startupbuffer * 1000;
 }
 
 if (argv.beta) {
@@ -99,6 +105,7 @@ class Bot {
         this.proc = spawn(cmd[0], cmd.slice(1));
         this.commands_sent = 0;
         this.command_callbacks = [];
+        this.firstmove = true;
 
         if (DEBUG) {
             this.log("Starting ", cmd.join(' '));
@@ -208,10 +215,14 @@ class Bot {
         let now = Date.now() - this.conn.clock_drift;
 
         if (state.clock.current_player == state.clock.black_player_id) {
-            black_offset = (now - state.clock.last_move) / 1000;
+            black_offset = ((this.firstmove==true ? argv.startupbuffer : 0) + now - state.clock.last_move) / 1000;
         } else {
-            white_offset = (now - state.clock.last_move) / 1000;
+            white_offset = ((this.firstmove==true ? argv.startupbuffer : 0) + now - state.clock.last_move) / 1000;
         }
+
+        // Only relevent with persistent bots
+        //
+        this.firstmove = false;
 
         if (state.time_control.system == 'byoyomi') {
             // GTP spec says time_left should have 0 for stones until main_time has run out.
