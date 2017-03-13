1592feb90613aae219838bccba41b211b05896a9
Jonathan Roy <jonroy7@gmail.com>
2017-03-13 19:20:35 -0400

    Expand usage line to match new README

--- gtp2ogs.js
+++ gtp2ogs.js
@@ -33,7 +33,7 @@ let console = require('tracer').colorConsole({
 
 
 let optimist = require("optimist")
-    .usage("Usage: $0 --username <bot-username> --apikey <apikey> -- command [arguments]")
+    .usage("Usage: $0 --username <bot-username> --apikey <apikey> [arguments] -- botcommand [bot arguments]")
     .alias('username', 'botid')
     .alias('username', 'bot')
     .alias('username', 'id')
