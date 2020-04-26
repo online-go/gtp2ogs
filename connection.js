b4777b70f65383394c171a9877c57690010372fa
wonderingabout <dota2orgoweiqibaduk@gmail.com>
2020-04-26 11:13:09 +0200

    fix

--- connection.js
+++ connection.js
@@ -344,10 +344,10 @@ class Connection {
     //
     checkChallengeBooleans(notification) {
 
-        if (config.rankedonly && notification.ranked) {
+        if (config.rankedonly && !notification.ranked) {
             return getBooleansGeneralReject("Ranked games are");
         }
-        if (config.unrankedonly && !notification.ranked) {
+        if (config.unrankedonly && notification.ranked) {
             return getBooleansGeneralReject("Unranked games are");
         }
 
