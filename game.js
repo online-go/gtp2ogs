3744bd75756d92078018028ac667b962afa01565
Jonathan Roy <jonroy7@gmail.com>
2020-05-11 07:32:13 -0400

    Move killing of bot after gamedataChanged earlier in code block

--- game.js
+++ game.js
@@ -41,11 +41,36 @@ class Game {
 
             const gamedataChanged = this.state ? (JSON.stringify(this.state) !== JSON.stringify(gamedata)) : false;
 
+            // If the gamedata is idential to current state, it's a duplicate. Ignore it and do nothing, unless
+            // bot is not running.
+            //
             if (this.state && !gamedataChanged && this.bot && !this.bot.dead) {
-                // If the gamedata is idential to current state, it's a duplicate. Ignore it and do nothing, unless no
-                // bot is running.
                 this.log('Ignoring gamedata that matches current state');
                 return;
+            }
+
+            // If server has issues it might send us a new gamedata packet and not a move event. We could try to
+            // check if we're missing a move and send it to bot out of gamedata. For now as a safe fallback just
+            // restart the bot by killing it here if another gamedata comes in. There normally should only be one
+            // before we process any moves, and makeMove() is where a new Bot is created.
+            //
+            if (this.bot && gamedataChanged) {
+                this.log("Killing bot because of gamedata change after bot was started");
+
+                if (config.DEBUG) {
+                    this.log('Previously seen gamedata:', this.state);
+                    this.log('New gamedata:', gamedata);
+                }
+
+                this.ensureBotKilled();
+
+                if (this.processing) {
+                    this.processing = false;
+                    --Game.moves_processing;
+                    if (config.corrqueue && this.state.time_control.speed === "correspondence") {
+                        --Game.corr_moves_processing;
+                    }
+                }
             }
 
             //this.log("Gamedata:", JSON.stringify(gamedata, null, 4));
@@ -81,30 +106,6 @@ class Game {
                     this.opponent_evenodd = this.state.moves.length % 2;
                 } else {
                     this.opponent_evenodd = (this.state.moves.length + 1) % 2;
-                }
-            }
-
-            // If server has issues it might send us a new gamedata packet and not a move event. We could try to
-            // check if we're missing a move and send it to bot out of gamedata. For now as a safe fallback just
-            // restart the bot by killing it here if another gamedata comes in. There normally should only be one
-            // before we process any moves, and makeMove() is where a new Bot is created.
-            //
-            if (this.bot && gamedataChanged) {
-                this.log("Killing bot because of gamedata change after bot was started");
-
-                if (config.DEBUG) {
-                    this.log('Previously seen gamedata:', this.state);
-                    this.log('New gamedata:', gamedata);
-                }
-
-                this.ensureBotKilled();
-
-                if (this.processing) {
-                    this.processing = false;
-                    --Game.moves_processing;
-                    if (config.corrqueue && this.state.time_control.speed === "correspondence") {
-                        --Game.corr_moves_processing;
-                    }
                 }
             }
 
