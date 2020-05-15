372eb0d15d64bff996efc30aa6080fa0cbf3ac42
Sander Land <sander.land@cognite.com>
2020-05-15 09:48:20 +0200

    fix gamedata

--- game.js
+++ game.js
@@ -39,6 +39,47 @@ class Game {
         this.socket.on(`game/${game_id}/gamedata`, (gamedata) => {
             if (!this.connected) return;
 
+            // Only call game over handler if game really just finished.
+            // For some reason we get connected to already finished games once in a while ...
+            if (gamedata.phase === 'finished') {
+                if (prev_phase && gamedata.phase !== prev_phase) this.gameOver();
+                return;
+            }
+
+            const gamedataChanged = this.state ? (JSON.stringify(this.state) !== JSON.stringify(gamedata)) : false;
+            // If the gamedata is idential to current state, it's a duplicate. Ignore it and do nothing, unless
+            // bot is not running.
+            //
+            if (this.state && !gamedataChanged && this.bot && !this.bot.dead) {
+                this.log('Ignoring gamedata that matches current state');
+                return;
+            }
+
+            // If server has issues it might send us a new gamedata packet and not a move event. We could try to
+            // check if we're missing a move and send it to bot out of gamedata. For now as a safe fallback just
+            // restart the bot by killing it here if another gamedata comes in. There normally should only be one
+            // before we process any moves, and makeMove() is where a new Bot is created.
+            //
+            if (this.bot && gamedataChanged) {
+
+                this.log("Killing bot because of gamedata change after bot was started");
+
+                if (config.DEBUG) {
+                    this.log('Previously seen gamedata:', this.state);
+                    this.log('New gamedata:', gamedata);
+                }
+
+                this.ensureBotKilled();
+
+                if (this.processing) {
+                    this.processing = false;
+                    --Game.moves_processing;
+                    if (config.corrqueue && this.state.time_control.speed === "correspondence") {
+                        --Game.corr_moves_processing;
+                    }
+                }
+            }
+
             //this.log("Gamedata:", JSON.stringify(gamedata, null, 4));
 
             const prev_phase = (this.state ? this.state.phase : null);
@@ -47,11 +88,6 @@ class Game {
             this.log(`gamedata     ${this.header()}`);
 
             this.conn.addGameForPlayer(gamedata.game_id, this.getOpponent().id);
-
-            // Only call game over handler if game really just finished.
-            // For some reason we get connected to already finished games once in a while ...
-            if (gamedata.phase === 'finished' && prev_phase && gamedata.phase !== prev_phase)
-                this.gameOver();
 
             // First handicap is just lower komi, more handicaps may change who is even or odd move #s.
             //
@@ -72,32 +108,6 @@ class Game {
                     this.opponent_evenodd = this.state.moves.length % 2;
                 } else {
                     this.opponent_evenodd = (this.state.moves.length + 1) % 2;
-                }
-            }
-
-            // If server has issues it might send us a new gamedata packet and not a move event. We could try to
-            // check if we're missing a move and send it to bot out of gamedata. For now as a safe fallback just
-            // restart the bot by killing it here if another gamedata comes in. There normally should only be one
-            // before we process any moves, and makeMove() is where a new Bot is created.
-            //
-            const gamedataChanged = (JSON.stringify(this.state) !== JSON.stringify(gamedata));
-
-            if (this.bot && gamedataChanged) {
-                this.log("Killing bot because of gamedata change after bot was started");
-
-                if (config.DEBUG) {
-                    this.log('Previously seen gamedata:', this.state);
-                    this.log('New gamedata:', gamedata);
-                }
-
-                this.ensureBotKilled();
-
-                if (this.processing) {
-                    this.processing = false;
-                    --Game.moves_processing;
-                    if (config.corrqueue && this.state.time_control.speed === "correspondence") {
-                        --Game.corr_moves_processing;
-                    }
                 }
             }
 
@@ -454,6 +464,7 @@ class Game {
         this.log(`Game over.   Result: ${col}+${res}  ${winloss}`);
 
         // Notify bot of end of game and send score
+        this.log("FAREWELL",config.farewellscore,!!this.bot)
         if (config.farewellscore && this.bot) {
             const sendTheScore = (score) => {
                 if (score) this.log(`Bot thinks the score was ${score}`);
