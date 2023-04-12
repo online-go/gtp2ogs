e95eacaff62fe0aad846debf7ea7cf5d41c82a43
Akita Noek <anoek@online-go.com>
2023-04-12 14:49:56 -0600

    Let jest run ts-jest in node_modules for goban

--- jest.config.ts
+++ jest.config.ts
@@ -189,9 +189,7 @@ export default {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-    "<rootDir>/node_modules/",
-  ],
+  transformIgnorePatterns: [ ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
